968fe544d28c7c55416bdb4753c48da2a72fc094
KUHAKU1221 <97268983+KUHAKU1221@users.noreply.github.com>
2022-11-28 11:42:27 +0900

    taiichiro

--- app/src/main/java/com/example/b_sket/MainActivity.kt
+++ app/src/main/java/com/example/b_sket/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.b_sket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.top)
+
+        val loginButton = findViewById<Button>(R.id.LoginButton)
+        loginButton.setOnClickListener{
+            val intent = Intent(this, ::class.java)
+            startActivity(intent)
+        }
     }
+
 }
