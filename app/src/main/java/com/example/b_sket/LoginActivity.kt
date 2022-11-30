8496279c40ca2064bf9179d1c16f3123e7d64b65
KUHAKU1221 <97268983+KUHAKU1221@users.noreply.github.com>
2022-11-30 14:13:36 +0900

    gaigai

--- app/src/main/java/com/example/b_sket/LoginActivity.kt
+++ app/src/main/java/com/example/b_sket/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.b_sket
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bsket.R
+import android.os.Bundle
+import android.widget.Button
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+
     }
 }
--- app/src/main/java/com/example/b_sket/MainActivity.kt
+++ app/src/main/java/com/example/b_sket/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.bsket.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
