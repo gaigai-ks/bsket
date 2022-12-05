60fe957679716fd765dca4ce39e64dd6374fd62b
KUHAKU1221 <97268983+KUHAKU1221@users.noreply.github.com>
2022-12-05 10:26:44 +0900

    ore

--- app/src/main/java/com/example/b_sket/AccountRegistActivity.kt
+++ app/src/main/java/com/example/b_sket/AccountRegistActivity.kt
@@ -1,6 +1,5 @@
 package com.example.b_sket
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,7 +7,7 @@ import android.widget.Button
 class AccountRegistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.accountregist)
+        setContentView(R.layout.account_regist)
 
         val accountRegistCancelButton = findViewById<Button>(R.id.AccountRegistCancelButton)
         accountRegistCancelButton.setOnClickListener{
