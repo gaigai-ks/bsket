ee18ac119db9548f7726604901681e429b68b9d7
KUHAKU1221 <97268983+KUHAKU1221@users.noreply.github.com>
2022-12-08 11:38:50 +0900

    ore

--- app/src/main/java/com/example/b_sket/MainActivity.kt
+++ app/src/main/java/com/example/b_sket/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.b_sket
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.b_sket.databinding.ActivityMain2Binding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.top)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
 
         /*loginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -17,5 +42,3 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AccountRegistActivity::class.java)
             startActivity(intent)
         }*/
-    }
-}
