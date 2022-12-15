c0639acba59a257c3aa9bf855f81373a5d23aaf5
KUHAKU1221 <97268983+KUHAKU1221@users.noreply.github.com>
2022-12-15 09:25:40 +0900

    ore

--- app/src/main/java/com/example/b_sket/MapsActivity.kt
+++ app/src/main/java/com/example/b_sket/MapsActivity.kt
@@ -1,19 +1,28 @@
 package com.example.b_sket
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.b_sket.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val closeButton = findViewById<Button>(R.id.close_Button)
+        closeButton.setOnClickListener {
+            finish()
+
+            fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this)
+
+        }
     }
 
     /**
@@ -38,17 +56,57 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.isMyLocationEnabled = true
 
-        // Add a marker in Sydney and move the camera
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        // 最後に確認された位置情報を取得
+        val fusedLocationClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.getLastLocation()
+            .addOnSuccessListener(this) { location ->
+                if (location != null) {
+
+
+                    // Add a marker in Sydney and move the camera
 //        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        val kyoto = LatLng(35.011665, 135.768326) //緯度・経度
-        mMap.addMarker(MarkerOptions().position(kyoto).title("Marker in Kyoto"))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kyoto, 8f)) //2)zoom表示
-        mMap.uiSettings.isZoomControlsEnabled = true //3)拡大・縮小ボタン表示
-
+                    val irai = LatLng(location.getLatitude(), location.getLongitude()) //緯度・経度
+                    mMap.addMarker(MarkerOptions().position(irai).title(" "))
+                    //mMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(irai, 17f)) //2)zoom表示
+                    googleMap.addPolyline(
+                        PolylineOptions()
+                            .add(irai)
+                            .color(Color.RED)                   // 線の色
+                            .width(8f)                          // 線の太さ
+                    )
+                }
+            }
     }
 }
