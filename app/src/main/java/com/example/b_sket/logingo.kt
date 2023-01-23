555365d377495ed46b93e769f7028935ef8a9d62
k4mitani <118332847+rin1025@users.noreply.github.com>
2023-01-23 16:44:22 +0900

    ore

--- app/src/main/java/com/example/b_sket/logingo.kt
+++ app/src/main/java/com/example/b_sket/logingo.kt
@@ -1,6 +1,5 @@
 package com.example.b_sket
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +8,46 @@ class logingo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val syousai = findViewById<Button>(R.id.syousai)
-//        syousai.setOnClickListener {
-//            val intent = Intent(this, AccountSyosai::class.java)
+        val add = findViewById<Button>(R.id.yousei)
+//        val listView = findViewById<ListView>(R.id.ListView)
+//        setListViewAdapter(listView)
+//
+//        add.setOnClickListener {
+//            val intent = Intent(this, a_text::class.java)
 //            startActivity(intent)
+//        }
+//
+//        listView.setOnItemClickListener { av, view, position, id ->
+//            val intent = Intent(this, a_text::class.java)
+//            val itemId = listView.adapter.getItemId(position)
+//            intent.putExtra("id", itemId)
+//            startActivity(intent)
+//        }
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        val helper = TestOpenHelper(this)
+//        val listView = findViewById<ListView>(R.id.ListView)
+//        setListViewAdapter(listView)
+//    }
+//
+//    fun setListViewAdapter(listView: ListView)
+//    {
+//        val helper = TestOpenHelper(this)
+//        helper.readableDatabase.use {
+//                db -> db.query("memos", arrayOf("id", "title", "content"),null,null,null,null,null,null)
+//            .use { cursor ->
+//                val memoList = mutableListOf<ListItem>()
+//                if (cursor.moveToFirst()) {
+//                    for (i in 1..cursor.count) {
+//                        val memoId = cursor.getInt(0)
+//                        val title = cursor.getString(1)
+//                        memoList.add(ListItem(memoId.toLong(), title))
+//                        cursor.moveToNext()
+//                    }
+//                }
+//                listView.adapter = CustomListAdapter(this, memoList, R.layout.list_item)
+//            }
         }
     }
-
-
